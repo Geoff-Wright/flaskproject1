c383630ce12095c2519a921ff8d22ec5f3111ab4
georgeBl <blanarugeorge96@gmail.com>
2023-01-31 11:18:09 +0000

    routes added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -6,6 +7,16 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+
+@app.route('/geoff/')
+def hello_geoff():  # put application's code here
+    return 'Hello Geoff!'
+
+
+@app.route('/capitalize/<word>/')
+def capitalize(word):
+    return '<h1>{}<h1>'.format(escape(word.capitalize()))
 
 
 if __name__ == '__main__':
